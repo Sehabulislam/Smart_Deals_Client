c29692746487afa1553e45299b017d27da238638
Sehabulislam <shehabulislam244@gmail.com>
2025-11-10 00:39:18 +0600

    first commit

---
+++ src/components/bids/BidForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BidForm = () => {
+    return (
+        <div>
+            <h3>bidsform</h3>
+        </div>
+    );
+};
+
+export default BidForm;
---
+++ src/components/bids/BidList.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const BidList = () => {
+    return (
+        <div>
+            <h1>BidList</h1>
+        </div>
+    );
+};
+
+export default BidList;
